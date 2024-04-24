5b08d2b4d4e4f059d58e5a24613b3d9660ec7ad6
Nikolay Dimov <114500017+NikolayDimov@users.noreply.github.com>
2024-04-24 14:55:39 +0300

    fwta(App): add useREduser and json server

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,57 @@ import DateCounter from "./Components/DateCounter";
 import { AppContainer, GlobalStyle, Root } from "./App.styled";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
+import { useEffect, useReducer } from "react";
+
+interface DataRecievedProps {
+    type: "dataReceived";
+    payload: [];
+}
+
+interface DataFailedProps {
+    type: "dataFailed";
+    // payload: [];
+}
+
+interface Question {
+    question: string;
+}
+
+interface State {
+    questions: Question[];
+    status: string;
+}
+
+type Action = DataRecievedProps | DataFailedProps;
+
+const initialState = {
+    questions: [],
+
+    // 'loading', 'error', 'ready', 'active', 'finished'
+    status: "loading",
+};
+
+function reducer(state: State, action: Action): State {
+    switch (action.type) {
+        case "dataReceived":
+            return { ...state, questions: action.payload, status: "ready" };
+        case "dataFailed":
+            return { ...state, status: "error" };
+        default:
+            throw new Error("Unknow action");
+    }
+}
 
 function App() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(function () {
+        fetch("http://localhost:9000/questions")
+            .then((res) => res.json())
+            .then((data) => dispatch({ type: "dataReceived", payload: data }))
+            .catch((error) => dispatch({ type: "dataFailed" }));
+    }, []);
+
     return (
         <Root>
             <GlobalStyle />
